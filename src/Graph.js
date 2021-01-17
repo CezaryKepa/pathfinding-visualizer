ccb44fdc13090c8b18b2f9b94215da8bd0dabf1c
Cezary Kępa <cezary.kepa@rsoftstudio.pl>
2021-01-17 12:30:19 +0100

    added dfs and bfs + refactor

--- src/Graph.js
+++ src/Graph.js
@@ -8,7 +8,7 @@ export class Graph {
     }
     addEdge(v, w) {
         this.AdjList.get(v).push(w);
-
+        this.AdjList.get(w).push(v);
     }
     printGraph()
     {
@@ -33,4 +33,65 @@ export class Graph {
         }
     }
 
+    async DFS(start, end) {
+        let stack = [start]
+        let path = []
+        const visited = {};
+
+        while (stack.length) {
+            let current = stack.pop()
+            visited[current] = true;
+            const elementById = document.getElementById(current);
+            elementById.style.background = '#757C81';
+            await sleep(1);
+            if(current === end) {
+                return path;
+            }
+            path.push(current)
+            const neighbours = this.AdjList.get(current);
+            if (this.AdjList.get(current).length > 0) {
+                for (const index in neighbours) {
+                    const element = neighbours[index];
+                    if (!visited[element])
+                        stack.push(element);
+                }
+            }
+        }
+
+        return path.reverse()
+    }
+
+    async BFS(start, end) {
+        let queue = [start]
+        let path = []
+        const visited = {};
+
+        while (queue.length) {
+            let current = queue.shift();
+            const elementById = document.getElementById(current);
+            elementById.style.background = '#757C81';
+            await sleep(1);
+            if(current === end) {
+                return path;
+            }
+            path.push(current)
+            const neighbours = this.AdjList.get(current);
+            if (this.AdjList.get(current).length > 0) {
+                for (const index in neighbours) {
+                    const element = neighbours[index];
+                    if (!visited[element]) {
+                        visited[element] = true;
+                        queue.push(element);
+                    }
+                }
+            }
+        }
+
+        return path;
+    }
+
+}
+
+export function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
