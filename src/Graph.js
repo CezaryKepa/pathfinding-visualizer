7945e1639625f7d32c58f76f7d3a919a0d18daec
CezaryKepa <max23273@gmail.com>
2021-01-17 14:20:39 +0100

    added dijkstra

--- src/Graph.js
+++ src/Graph.js
@@ -1,26 +1,26 @@
 export class Graph {
     constructor(noOfVertices) {
-        this.noOfVertices = noOfVertices;
-        this.AdjList = new Map();
+        this.adjList = new Map();
     }
+
     addVertex(v) {
-        this.AdjList.set(v, []);
+        this.adjList.set(v, []);
     }
+
     addEdge(v, w) {
-        this.AdjList.get(v).push(w);
-        this.AdjList.get(w).push(v);
+        this.adjList.get(v).push(w);
+        this.adjList.get(w).push(v);
     }
-    printGraph()
-    {
+
+    printGraph() {
         // get all the vertices
-        const get_keys = this.AdjList.keys();
+        const get_keys = this.adjList.keys();
 
         // iterate over the vertices
-        for (const i of get_keys)
-        {
+        for (const i of get_keys) {
             // great the corresponding adjacency list
             // for the vertex
-            const get_values = this.AdjList.get(i);
+            const get_values = this.adjList.get(i);
             let conc = "";
 
             // iterate over the adjacency list
@@ -44,12 +44,12 @@ export class Graph {
             const elementById = document.getElementById(current);
             elementById.style.background = '#757C81';
             await sleep(1);
-            if(current === end) {
+            if (current === end) {
                 return path;
             }
             path.push(current)
-            const neighbours = this.AdjList.get(current);
-            if (this.AdjList.get(current).length > 0) {
+            const neighbours = this.adjList.get(current);
+            if (this.adjList.get(current).length > 0) {
                 for (const index in neighbours) {
                     const element = neighbours[index];
                     if (!visited[element])
@@ -71,12 +71,12 @@ export class Graph {
             const elementById = document.getElementById(current);
             elementById.style.background = '#757C81';
             await sleep(1);
-            if(current === end) {
+            if (current === end) {
                 return path;
             }
             path.push(current)
-            const neighbours = this.AdjList.get(current);
-            if (this.AdjList.get(current).length > 0) {
+            const neighbours = this.adjList.get(current);
+            if (this.adjList.get(current).length > 0) {
                 for (const index in neighbours) {
                     const element = neighbours[index];
                     if (!visited[element]) {
@@ -90,8 +90,70 @@ export class Graph {
         return path;
     }
 
-}
+    dijkstra(start, end) {
+        // Init helper variables that we will need for Dijkstra algorithm.
+        const distances = {};
+        const visitedVertices = {};
+        const previousVertices = {};
+        const queue = new PriorityQueue();
 
+        // Init all distances with infinity assuming that currently we can't reach
+        // any of the vertices except the start one.
+        this.adjList().forEach((vertex) => {
+            distances[vertex.getKey()] = Infinity;
+            previousVertices[vertex.getKey()] = null;
+        });
+
+        // We are already at the startVertex so the distance to it is zero.
+        distances[start.getKey()] = 0;
+
+        // Init vertices queue.
+        queue.add(start, distances[start.getKey()]);
+
+        // Iterate over the priority queue of vertices until it is empty.
+        while (!queue.isEmpty()) {
+            // Fetch next closest vertex.
+            const currentVertex = queue.poll();
+
+            // Iterate over every unvisited neighbor of the current vertex.
+            const neighbors = this.adjList.get(currentVertex);
+            neighbors.forEach((neighbor) => {
+                // Don't visit already visited vertices.
+                if (!visitedVertices[neighbor.getKey()]) {
+                    // Update distances to every neighbor from current vertex.
+                    // const edge = graph.findEdge(currentVertex, neighbor);
+
+                    const existingDistanceToNeighbor = distances[neighbor.getKey()];
+                    // const distanceToNeighborFromCurrent = distances[currentVertex.getKey()] + edge.weight;
+                    const distanceToNeighborFromCurrent = distances[currentVertex.getKey()] + 1;
+
+                    // If we've found shorter path to the neighbor - update it.
+                    if (distanceToNeighborFromCurrent < existingDistanceToNeighbor) {
+                        distances[neighbor.getKey()] = distanceToNeighborFromCurrent;
+
+                        // Change priority of the neighbor in a queue since it might have became closer.
+                        if (queue.hasValue(neighbor)) {
+                            queue.changePriority(neighbor, distances[neighbor.getKey()]);
+                        }
+
+                        // Remember previous closest vertex.
+                        previousVertices[neighbor.getKey()] = currentVertex;
+                    }
+
+                    // Add neighbor to the queue for further visiting.
+                    if (!queue.hasValue(neighbor)) {
+                        queue.add(neighbor, distances[neighbor.getKey()]);
+                    }
+                }
+            });
+
+            // Add current vertex to visited ones to avoid visiting it again later.
+            visitedVertices[currentVertex.getKey()] = currentVertex;
+        }
+
+
+    }
+}
 export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
